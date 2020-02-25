6a62e532a42435b5395aa108984f53c0fac6e160
halil ibrahim turan <halilbrhmtrn@gmail.com>
2020-02-26 00:30:47 +0300

    refactoring parkinglotRoute

--- src/routes/parkingLotRoute.js
+++ src/routes/parkingLotRoute.js
@@ -1,10 +1,9 @@
-const express = require('express');
-const parkingLotController = require('../controllers/parkingLot.controller');
+const express = require("express");
+const parkingLotController = require("../controllers/parkingLot.controller");
 const router = express.Router();
 
-router.get('/list', parkingLotController.listParkingLots);
-router.get('/add', parkingLotController.addParkingLotView);
-router.post('/add', parkingLotController.addNewParkingLot);
-
+router.get("/list", parkingLotController.listParkingLots);
+router.get("/add", parkingLotController.addParkingLotView);
+router.post("/add", parkingLotController.addNewParkingLot);
 
 module.exports = router;
