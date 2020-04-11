d996af2c0c3a26965644e60727734184ba756b00
Cafer Bahar <cbahar955@gmail.com>
2020-04-11 15:23:04 +0300

    Changed exit screen

--- src/hbsHelpers/index.js
+++ src/hbsHelpers/index.js
@@ -2,6 +2,7 @@ const hbsHelpers = require('./helpers');
 
 module.exports = {
   ifEquals: hbsHelpers.ifEquals,
+  ifNotEquals: hbsHelpers.ifNotEquals,
   incrementByOne: hbsHelpers.incrementByOne,
   ifIdInArr: hbsHelpers.ifIdInArr,
   isUserCompetencyIncludesRoute: hbsHelpers.isUserCompetencyIncludesRoute,
